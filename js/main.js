5015334d3cae2101f59fe8fa11e9eae3667503f2
AlfaDante <47882936+AlfaDante@users.noreply.github.com>
2019-03-21 16:48:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,11 +13,13 @@ $(function(){
     linky.forEach(function(obj, idx){
         odkazy.append('<a href="'+obj.link+'" target="_blank"><img src="img/'+obj.photo+'"></a>');
     });
+    
     var mista =$('#postava'); 
     var x=0;
     window.setInterval(function(){
         mista.find('img').attr('src','img/' + postavy[x].photo);
         mista.find('figcaption').text(postavy[x].name);
+        mista.find('p').text(postavy[x].desc);
         x == postavy.length -1 ? x=0 : x++;
     },4000);
 
